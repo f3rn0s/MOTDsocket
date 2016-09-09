b2131c92dfd4c4495055eaafdc02965cf2e9851b
Teal <tealkine2@gmail.com>
2016-09-09 14:49:35 +1000

    Updated

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 '''
-A simple getter server, client connects and can issue text commands to recieve
-files.
+A simple getter server, client connects and can issue text commands to recieve files.
 '''
 import socket
 import sys
@@ -10,23 +9,41 @@ import datetime
 from thread import *
 from urllib2 import urlopen
 
-now = datetime.datetime.now()
-
 HOST = ''   # Symbolic name meaning all available interfaces
-PORT = 31337 # Arbitrary non-privileged port
+while 1:
+	try:
+		PORT = int(raw_input("Port: ")) # Arbitrary non-privileged port
+		if PORT <= 65535:	
+			break
+		else:
+			print("Enter a valid port please (1-65535)")
+	except:
+		print("Enter a valid port please (1-65535)")
+
+def makedir(foldername):
+	if not os.path.exists(foldername):
+		os.makedirs(foldername)
+
+
+#Creates directories
+makedir('logs')
+makedir('private')
+makedir('public')
+
+#Creates log file with time
+now = datetime.datetime.now()
 TIME = now.strftime('%Y-%m-%d-%H:%M')
+LOGFILE = "logs/log-" + TIME + ".txt"
 
-LOGFILE = "log-" + TIME + ".txt"
-
+#Attempts to create all sockets
 try:
 	my_ip = urlopen('http://ip.42.pl/raw').read()
 	ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	ss.connect(('google.com', 0))
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except:
 	print("No interent connection")	
 	exit()
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print('Socket created')
  
@@ -41,22 +58,31 @@ print('Socket bind complete')
  
 #Start listening on socket
 s.listen(10)
+
+#Gives information about serveri
 print('Socket now listening on port: ' + str(PORT))
-#print('Public IP Address: ' + str(my_ip))
+print('Public IP Address: ' + str(my_ip))
 print('Private IP Address: ' + str(ss.getsockname()[0]))
 
-
+#Some useful functions
 def clearscreen(conn):
 	conn.send('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+
+def ignore():
+	ignore = 'ignore'
 
 #Function for handling connections. This will be used to create threads
 def clientthread(conn):
 	
+	#stores relevant information about connection
 	user = addr[0]
 	port = str(addr[1])
 
+	#Welcomes user to the server
 	conn.send('Welcome to the server, type /motd to get started\n')
         conn.send('Type quit to leave the server at any time\n')
+	
+	#Places user in a loop until disconnect
 	while True:
 		try:
 			data = str(conn.recv(1024))
@@ -67,13 +93,16 @@ def clientthread(conn):
 		
 		if data.lower() == '/clear\n':
 			clearscreen(conn)
-		if data.lower() == '/motd\n':
+		elif data.lower() == '/motd\n':
 			conn.send('\n')
-			f = open('motd.txt', 'r')
-			for line in f:
-				conn.send(line)
-			f.close()
-		if data.lower() == "/login\n":
+			try:
+				f = open('motd.info', 'r')
+				for line in f:
+					conn.send(line)
+				f.close()
+			except:
+				conn.send('server is missing a motd.info file\n')
+		elif data.lower() == "/login\n":
 			conn.send('Any sniffing of this network traffic will show your password in clear text\n')
 			conn.send('\nPassword: ')
 			try:
@@ -83,31 +112,40 @@ def clientthread(conn):
 				break
 			if passworddata == 'password\n':
 				clearscreen(conn)
-				conn.send('Goodjob\n')
+				conn.send('Goodjob, you can now access authorised only files\n')
+				conn.send('Make sure to type quit before leaving!')
+				while 1:
+					securedata = str(conn.recv(1024))
+					if securedata.lower() == "quit\n" or securedata.lower() == "/quit\n":
+						break
+					else:
+						try:
+							f = open("private/" + (securedata.lower()).rstrip() + ".txt", 'r')
+							for line in f:
+								conn.send(line)
+							f.close()
+						except:
+							conn.send("File does not exist\n")
 			else:
 				clearscreen(conn)
 				conn.send('Incorrect\n')
 				with open(LOGFILE, "a") as myfile: 
 					myfile.write('Failed login attempt by: ' + user + ':' + port + '\n')
 				print('Failed login attempt by: ' + user + ':' + port)
-		
-		#Temporary solution for opening files
-		if data.lower() == "info\n":
-			conn.send('\n')
-			f = open('info.txt', 'r')
-			for line in f:
-				conn.send(line)
-			f.close()
-		if data.lower() == "cyberpatriot\n":
-			conn.send('\n')
-			f = open('cyberpatriot.txt', 'r')
-			for line in f:
-				conn.send(line)
-			f.close()
-		if data.lower() == "quit\n":
-			conn.close()
-			print('User ' + user + ':' + port + ' disconnected')
-			break
+                elif data.lower() == "quit\n" or data.lower() == "/quit\n":
+                        conn.close()
+                        print('User ' + user + ':' + port + ' disconnected')
+                        break
+		#Solution for opening files
+		else:
+			try:
+				f = open("public/" + (data.lower()).rstrip() + ".txt", 'r')
+				for line in f:
+					conn.send(line)
+				f.close()
+			except:
+				ignore()
+
 while 1: 
 	#wait to accept a connection - blocking call
 	conn, addr = s.accept()
